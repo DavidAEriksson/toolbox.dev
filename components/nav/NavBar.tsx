59291822ce484ccbfc8859fea8c756ac03a1c3b2
David <david.umea3303@gmail.com>
2022-07-20 09:57:41 +0200

    feat(navbar): add primitive attempt to nav item children

--- components/nav/NavBar.tsx
+++ components/nav/NavBar.tsx
@@ -4,6 +4,7 @@ import { AiOutlineFieldString, AiOutlineFieldBinary } from 'react-icons/ai'
 import { VscJson } from 'react-icons/vsc'
 import React, { useState } from "react";
 import NavItem from "./NavItem";
+import NavItemSub from "./NavItemSub";
 
 export default function NavBar(): JSX.Element {
   const [navWidth, setNavWidth] = useState<string>('15vw')
@@ -11,11 +12,13 @@ export default function NavBar(): JSX.Element {
   return (
     <Flex flexDirection="column" transition="width 0.2s ease-in" w={navWidth} h="100vh" backgroundColor="gray.250">
       {navWidth === '15vw' ? 
-        <CloseIcon w={5} h={5} transition="0.3s" alignSelf="flex-end" mt="4" mr="4" _hover={{color: "red.50"}} onClick={() => setNavWidth('5vw')}/>
-        : <ArrowRightIcon w={5} h={5} transition="0.3s" alignSelf="flex-end" mt="4" mr="4" _hover={{color: "green.100"}} onClick={() => setNavWidth('15vw')}/>
+        <CloseIcon w={6} h={6} p="2" borderRadius="3" backgroundColor="gray.500" transition="0.3s" alignSelf="flex-end" mt="4" mr="4" _hover={{color: "red.50"}} onClick={() => setNavWidth('5vw')}/>
+        : <ArrowRightIcon w={6} h={6} p="2" borderRadius="3" backgroundColor="gray.500" transition="0.3s" alignSelf="flex-end" mt="4" mr="4" _hover={{color: "green.100"}} onClick={() => setNavWidth('15vw')}/>
       }
       <Flex mt="10"/>
-      <NavItem route="/strings" name="Strings" icon={AiOutlineFieldString}/>
+      <NavItem route="/strings" name="Strings" icon={AiOutlineFieldString}>
+        <NavItemSub route="/strings/test" name="child test" icon={AiOutlineFieldString} />
+      </NavItem>
       <NavItem route="/numbers" name="Numbers" icon={AiOutlineFieldBinary}/>
       <NavItem route="/json" name="JSON" icon={VscJson}/>
     </Flex>
--- components/nav/NavItemSub.tsx
+++ components/nav/NavItemSub.tsx
@@ -4,33 +4,30 @@ import { Icon } from '@chakra-ui/react'
 import { IconType } from "react-icons";
 import { ReactNode } from "react";
 
-type NavItemProps = {
+type NavItemSubProps = {
   route: string
   name: string
   icon: IconType
   children?: ReactNode
 }
 
-export default function NavItem({ route, name, icon, children }: NavItemProps){
+export default function NavItem({ route, name, icon}: NavItemSubProps){
   return (
    <Flex 
      pl="2" 
      pt="4"
      pb="4"
-     flexDirection="column"
+     alignItems="center"
      backgroundColor="gray.550"
      borderBottom="solid 2px"
      borderColor="white.50"
      _hover={{ color: "green.100", backgroundColor: 'gray.600', borderColor: 'green.100' }}
      transition="0.2s"
      cursor="pointer" >
-      <Flex>
-        <Icon mr="4" w={6} h={6} as={icon} />
-        <Link href={route}>
-          <Text fontWeight="bold" fontSize="xl">{name}</Text >
-        </Link>
-      </Flex>
-      { children }
+       <Icon mr="4" w={6} h={6} as={icon} />
+       <Link href={route}>
+         <Text fontWeight="bold" fontSize="xl">{name}</Text >
+       </Link>
    </Flex>
   );
 }
